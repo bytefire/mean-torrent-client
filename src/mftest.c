84bb0ff003436312b9b1fc2522def29a67008ac9
bytefire <okash.khawaja@gmail.com>
2014-04-20 11:19:43 +0100

    mftest.c now outputs the sha1 of info dictionary.

--- src/mftest.c
+++ src/mftest.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
 {
 	uint8_t *hash;
 	struct metafile_info mi;
+	int i;
 
 	if(read_metafile(FILE_NAME, &mi) != 0)
 	{
@@ -21,9 +22,16 @@ int main(int argc, char *argv[])
 	printf("Metafile Info:\n");
 	metafile_print(&mi);
 
+	printf("\nSHA1 of info dictionary: ");
 	hash = sha1_compute(mi.info_val, mi.info_len);
-
+	for(i=0; i<20; i++)
+	{
+		printf("%02x", hash[i]);
+	}	
+	printf("\n");
+	
 	metafile_free(&mi);
+	free(hash);
 
 	return 0;
 }
